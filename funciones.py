48c8e1e7c029a475a89e5303cb14311e31f4bf1e
lcgalans2024 <lcgalans2024@gmail.com>
2025-06-03 21:29:12 -0500

    Se agregan funciones de style

--- funciones.py
+++ funciones.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 @st.cache_data
 def cargar_datos_all(file_path, nombre_hoja):
@@ -29,3 +30,56 @@ def color_calificacion(val):
     else:
         color = 'background-color: #ff0000; color: white'  # Rojo
     return color
+
+def color_condicional(val):
+    if val == 1:
+        color = 'background-color: #00b050; color: black'  # Verde
+    else:
+        color = 'background-color: #ff0000; color: white'  # Rojo
+    return color
+
+# Función para aplicar el estilo condicional
+def resaltar_aciertos(row):
+    if row['ACIERTOS'] == 1:
+        return ['background-color: lightgreen', '', '']
+    else:
+        return ['', '', '']
+    
+def resaltar_aciertos1(row):
+    styles = []
+    if row['ACIERTOS'] == 1:
+        styles.append('background-color: lightgreen')  # PREGUNTA
+    else:
+        styles.append('')  # PREGUNTA
+    styles.append('')  # CATEGORIA
+    styles.append('')  # ACIERTOS (aunque se ocultará)
+    return styles
+
+@st.cache_data
+# Create a horizontal bar plot representing score
+def barra_progreso(M,nota,meta,titulo='Nota'):
+    fig, ax = plt.subplots(figsize=(8, 2))
+    # Create the bar
+    ax.broken_barh([(0, M)], (0.5, 1), facecolors='lightgray')
+    ax.broken_barh([(0, nota)], (0.5, 1), facecolors='lightgreen')
+
+    # Add a vertical line for the score
+    ax.vlines(meta, 0.5, 1.5, colors='black')
+
+    # Colocar el valor de la nota dentro de la barra verde
+    ax.text(nota, 0.95, f'{nota}', ha='right', va='center', fontsize=12, color='black', fontweight='bold')
+
+    # Personalizar el gráfico
+    ax.set_yticks([])
+    ax.set_xticks(range(0, M + 1))
+    ax.set_xlim(0, M)
+    ax.set_ylim(0, 2)
+    ax.set_title(titulo)
+
+    # Mostrar los bordes del gráfico
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.spines['bottom'].set_visible(True)
+
+    return fig
