827529e2db1848d094d49334a96c5636536f672c
lcgalans2024 <lcgalans2024@gmail.com>
2025-07-27 22:59:58 -0500

    Se activan los materiales del periodo 2

--- App_V2/components/materiales.py
+++ App_V2/components/materiales.py
@@ -10,40 +10,41 @@ def mostrar():
     #st.header("📎 Material del área y comunicados")
     # condicionar con periodo y grupo
     if st.session_state.periodo1 == "1":
-        st.subheader(f"Periodo {st.session_state.periodo1} - Grupo {st.session_state.grupo1}")
-        #st.subheader("📂 Materiales disponibles")
-        st.markdown("- [Eva_Operaciones_Básicas](#)")
-        with open("materiales/Eva_Operaciones_Básicas_VA.pdf", "rb") as file:
-            st.download_button(
-                label="- [📥 Descargar Eva_Operaciones_Básicas](#)",
-                data=file,
-                file_name="Eva_Operaciones_Básicas_VA.pdf",
-                mime="application/pdf"
-            )
-        st.markdown("[📥 Descargar Guía de Álgebra 10°](ENLACE_DE_DESCARGA_DIRECTA)")
-        st.subheader("📄 Vista previa - Guía de Álgebra 10°")
-        mostrar_pdf("materiales/Eva_Operaciones_Básicas_VA.pdf")
-        st.markdown("- 📝 [Autoevaluación](#)")
-        st.markdown("[👉 Ir a la Autoevaluación](https://forms.office.com/r/Mpy6gSrerC)", unsafe_allow_html=True)
-        st.subheader("📱 Autoevaluación - escanea el QR")
-        qr = qrcode.make("https://forms.office.com/r/Mpy6gSrerC")
-        buf = BytesIO()
-        qr.save(buf, format="PNG")
-        st.image(buf.getvalue(), caption="Escanea para abrir la autoevaluación")
-        st.subheader("📝 Autoevaluación integrada")
-        components.html(
-            '''
-            <iframe width="640px" height="480px" 
-            src="https://forms.office.com/r/Mpy6gSrerC?embed=true" 
-            frameborder="0" marginwidth="0" marginheight="0" 
-            style="border: none; max-width:100%; max-height:100vh" 
-            allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> 
-            </iframe>
-            ''',
-            height=500,
-        )
-        st.subheader("📢 Comunicaciones importantes")
-        st.info("Semana de evaluaciones del 20 al 24 de junio.")
+        st.write("Actividades no disponibles")
+        #st.subheader(f"Periodo {st.session_state.periodo1} - Grupo {st.session_state.grupo1}")
+        ##st.subheader("📂 Materiales disponibles")
+        #st.markdown("- [Eva_Operaciones_Básicas](#)")
+        #with open("materiales/Eva_Operaciones_Básicas_VA.pdf", "rb") as file:
+        #    st.download_button(
+        #        label="- [📥 Descargar Eva_Operaciones_Básicas](#)",
+        #        data=file,
+        #        file_name="Eva_Operaciones_Básicas_VA.pdf",
+        #        mime="application/pdf"
+        #    )
+        #st.markdown("[📥 Descargar Guía de Álgebra 10°](ENLACE_DE_DESCARGA_DIRECTA)")
+        #st.subheader("📄 Vista previa - Guía de Álgebra 10°")
+        #mostrar_pdf("materiales/Eva_Operaciones_Básicas_VA.pdf")
+        #st.markdown("- 📝 [Autoevaluación](#)")
+        #st.markdown("[👉 Ir a la Autoevaluación](https://forms.office.com/r/Mpy6gSrerC)", unsafe_allow_html=True)
+        #st.subheader("📱 Autoevaluación - escanea el QR")
+        #qr = qrcode.make("https://forms.office.com/r/Mpy6gSrerC")
+        #buf = BytesIO()
+        #qr.save(buf, format="PNG")
+        #st.image(buf.getvalue(), caption="Escanea para abrir la autoevaluación")
+        #st.subheader("📝 Autoevaluación integrada")
+        #components.html(
+        #    '''
+        #    <iframe width="640px" height="480px" 
+        #    src="https://forms.office.com/r/Mpy6gSrerC?embed=true" 
+        #    frameborder="0" marginwidth="0" marginheight="0" 
+        #    style="border: none; max-width:100%; max-height:100vh" 
+        #    allowfullscreen webkitallowfullscreen mozallowfullscreen msallowfullscreen> 
+        #    </iframe>
+        #    ''',
+        #    height=500,
+        #)
+        #st.subheader("📢 Comunicaciones importantes")
+        #st.info("Semana de evaluaciones del 20 al 24 de junio.")
     #####################################################################################################
     elif st.session_state.periodo1 == "2":
         st.subheader(f"Periodo {st.session_state.periodo1} - Grupo {st.session_state.grupo1}")
