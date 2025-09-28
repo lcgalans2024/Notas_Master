408c2ca0764ff1252a2c29fae7e57466b1f9e541
lcgalans2024 <lcgalans2024@gmail.com>
2025-09-28 00:04:52 -0500

    Se genera tabla merge de consolidados

--- App_V2/components/informe.py
+++ App_V2/components/informe.py
@@ -9,7 +9,23 @@ def mostrar_informe():
     """
     #st.title("Informe de Notas")
 
-    # Cargar datos de notas
+    # Cargar consolidados
+    st.session_state.consolidado_P1 = load_hoja_google_consolidados(st.session_state.SHEET_ID_CONSOLIDADOS, st.session_state.GIDS_CONSOLIDADOS, f'{st.session_state.grupo1}_P1')
+    # procesar consolidados P1
+    st.session_state.consolidado_P1 = procesar_consolidados(st.session_state.consolidado_P1)
+    # cargar consolidados P2  
+    st.session_state.consolidado_P2 = load_hoja_google_consolidados(st.session_state.SHEET_ID_CONSOLIDADOS, st.session_state.GIDS_CONSOLIDADOS, f'{st.session_state.grupo1}_P2')
+    # procesar consolidados P2
+    st.session_state.consolidado_P2 = procesar_consolidados(st.session_state.consolidado_P2)
+    # Unificar los consolidados P1 y P2 en un solo dataframe con metodo merge
+    st.session_state.consolidado_P1_P2 = st.session_state.consolidado_P1.merge(st.session_state.consolidado_P2, on=['MATERIA'], how='outer', suffixes=('_P1', '_P2'))
+    # renombrar columnas Nota_P1 a PERÍODO 1 y Nota_P2 a PERÍODO 2
+    st.session_state.consolidado_P1_P2 = st.session_state.consolidado_P1_P2.rename(columns={'NOTA_P1': 'PERÍODO 1', 'NOTA_P2': 'PERÍODO 2'})
+
+    
+
+
+    # Cargar consolidado segun el periodo seleccionado
     df_consolidados = load_hoja_google_consolidados(st.session_state.SHEET_ID_CONSOLIDADOS, st.session_state.GIDS_CONSOLIDADOS, f'{st.session_state.grupo1}_P{st.session_state.periodo1}')
 
     # Procesar los datos de notas
